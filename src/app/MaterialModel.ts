678f294107ce1494c9de18cc73167021bc8ebaa0
castillag18 <orlandojs199918@gmail.com>
2019-09-23 22:10:37 -0500

    sync

--- src/app/MaterialModel.ts
+++ src/app/MaterialModel.ts
@@ -6,14 +6,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
 
 import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule, 
-    MatFormFieldModule, MatSelectModule, MatInputModule, MatBadgeModule],
+    MatFormFieldModule, MatSelectModule, MatInputModule, MatBadgeModule, MatCardModule],
   exports: [MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule, 
-    MatFormFieldModule, MatSelectModule, MatInputModule, MatBadgeModule],
+    MatFormFieldModule, MatSelectModule, MatInputModule, MatBadgeModule, MatCardModule],
 })
 
 
